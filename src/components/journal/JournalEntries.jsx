baba428db9574d9346216940f5d527e909f49305
eudycolmenares <eudy.developer@gmail.com>
2022-05-03 19:30:42 -0500

    Some components and their styles are created

--- src/components/journal/JournalEntries.jsx
+++ src/components/journal/JournalEntries.jsx
@@ -1,7 +1,8 @@
 import { JournalEntry } from "./JournalEntry"
 
 export const JournalEntries = () => {
-  const entrie = [1,2,3,4,5,6]
+  const entrie = [1,2,3,4,5,6,7,8,9,10,11]
+  // const entrie = [1,2,3,4,5,6]
   return (
     <div className="journal__entries">
       {
